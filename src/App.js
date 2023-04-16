ff1f89490a53c24fa1d90de7ff64e62f08fa8051
abhishek yadav <abhishekyadav9667@gmail.com>
2023-04-16 13:56:45 +0530

    project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './Component/Header/Header';
+import Gallary from './Component/Gallary/Gallary';
+
+function App() {
+  return (
+    <div className="App">
+     <Header/>
+     <Gallary/>
+    </div>
+  );
+}
+
+export default App;
